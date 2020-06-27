7f3bd291db3eb299239667b3eae72798f71b9141
Angélique Faye <faye.angelique.d@gmail.com>
2020-06-27 16:36:31 +0200

    bugs, paths

--- gulpfile.js
+++ gulpfile.js
@@ -18,13 +18,35 @@ var rename = require('gulp-rename');
 var terser = require('gulp-terser');
 //live reload
 const browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+const paths = {
+    html: {
+        index: './src/*.html',
+        dest: './dist/'
+    },
+    css: {
+        src: './src/scss/style.scss',
+        allscss: './src/scss/**/*.scss',
+        dest: './dist/css/'
+    },
+    js: {
+        src: './src/js/*.js',
+        alljs: 'src/js/**/*.js',
+        dest: './dist/js/'
+    },
+    php: {
+        index: 'index.php',
+        appFiles: './**/*.php'
+    }
+}
 
 function html() {
-    return gulp.src('./src/index.html')
+    return gulp.src(paths.html.index)
         //replace file paths in html
         .pipe(htmlreplace({
-            'css': 'style.min.css',
-            'js': 'script.min.js'
+            'css': './css/style.min.css',
+            'js': './js/script.min.js'
         },
             {
                 keepBlockTags: false,
@@ -32,51 +54,51 @@ function html() {
         ))
         //minify html
         .pipe(htmlmin({ collapseWhitespace: true }))
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest(paths.html.dest))
         .pipe(browserSync.stream());
 }
-function style() {
+function css() {
     var plugins = [
         //config in .browserslistrc
         autoprefixer(),
         cssnano()
     ];
 
-    return gulp.src('./src/scss/style.scss')
+    return gulp.src(paths.css.src)
         .pipe(sass())
         .pipe(postcss(plugins))
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest(paths.css.dest))
         .pipe(browserSync.stream());
 }
-function scripts() {
+function js() {
     //minify
     return gulp.src('./src/js/script.js')
         .pipe(terser())
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./dist/js'))
-        .pipe(browserSync.reload())
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(browserSync.stream())
 }
 function watch() {
     browserSync.init({
         server: {
-            baseDir: "./src",
+            baseDir: "./dist",
             index: "index.html"
         }
     });
     browserSync.reload()
-    gulp.watch('src/*.html', html);
-    gulp.watch('./src/*.html').on('change', browserSync.reload);
-    gulp.watch('src/scss/**/*.scss', style);
-    gulp.watch('src/scss/**/*.scss').on('change', browserSync.reload);
-    gulp.watch('src/js/**/*.js', scripts);
-    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch(paths.html.index, html);
+    gulp.watch(paths.html.index).on('change', reload);
+    gulp.watch(paths.css.allscss, css);
+    gulp.watch(paths.css.allscss).on('change', reload);
+    gulp.watch(paths.js.alljs, js);
+    gulp.watch(paths.js.alljs).on('change', reload);
 }
 exports.html = html;
-exports.style = style;
-exports.scripts = scripts;
+exports.css = css;
+exports.js = js;
 exports.watch = watch;
